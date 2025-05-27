ffb4838d40e9b6e4b98dc99f32553dbeb4630dd4
yairsebastianmora <sebitasmax1@gmail.com>
2025-05-27 17:27:09 -0500

    Corrijo doble ejecución de app.run()

--- app.py
+++ app.py
@@ -451,8 +451,5 @@ def ver_tarea(tarea_id):
                 flash('Debes seleccionar un archivo para entregar.')
     return render_template('ver_tarea.html', tarea=tarea, hoy=hoy)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
